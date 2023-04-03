bdede1e8ff05883e569b84a4e74f1f2136ea44f6
Nuno Vieira <nunovieira220@gmail.com>
2023-04-03 17:12:55 +0100

    Improve comment indentation

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 /*
  * Dependencies
-*/
+ */
 
-import { GeneratorOptions, GeneratorResponse } from '/interfaces';
+import { GeneratorOptions, GeneratorResponse } from 'lib';
 import { getErrorResponse } from '/utils/general-util';
 import generators from '/generators/index';
 
 /*
  * Export main function
-*/
+ */
 
 export default (teams: string[], options: GeneratorOptions): GeneratorResponse => {
   const generator = generators[options.type];
